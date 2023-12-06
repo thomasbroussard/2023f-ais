98fd9491a017f1812faf1e71232ec4ac79d0836b
Présentation <tbr@tbr.com>
2023-12-06 16:07:12 +0100

    * adding the biostat solution template

---
+++ file-loading-exercise/src/test/java/TestBarChart.java
@@ -0,0 +1,29 @@
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.CategoryChartBuilder;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.style.Styler;
+
+import java.util.Arrays;
+
+public class TestBarChart {
+
+    public static void main(String[] args) {
+        CategoryChart chart = getChart();
+        new SwingWrapper<CategoryChart>(chart).displayChart();
+    }
+
+
+    public static CategoryChart getChart() {
+
+        // Create Chart
+        CategoryChart chart = new CategoryChartBuilder().width(800).height(600).title("Score Histogram").xAxisTitle("Score").yAxisTitle("Number").build();
+
+        // Customize Chart
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+
+        // Series
+        chart.addSeries("test 1", Arrays.asList(new Integer[] { 0, 1, 2, 3, 4 }), Arrays.asList(new Integer[] { 4, 5, 9, 6, 5 }));
+
+        return chart;
+    }
+}
